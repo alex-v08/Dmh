67c9e7ea3eb230b2e6745b1996034441ac24a7d1
alex-v08 <alex.velasquez08@outlook.com>
2024-06-25 11:14:09 -0300

    minor fix

--- UserService/src/main/java/com/dmh/UserService/controller/UserController.java
+++ UserService/src/main/java/com/dmh/UserService/controller/UserController.java
@@ -9,21 +9,20 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
-
 @RequiredArgsConstructor
-
-public class UserRepository {
+public class UserController {
     private final UserServiceImpl userService;
 
     @PostMapping
     public ResponseEntity<String> createUser(@RequestBody UserDTO userDTO) {
-       try {
-           userService.createUser(userDTO);
-           return new ResponseEntity<>("User created", HttpStatus.CREATED);
-       } catch (Exception e) {
-           return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-       }
+        try {
+            userService.createUser(userDTO);
+            return new ResponseEntity<>("User created", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
+
     @PutMapping("/{id}")
     public ResponseEntity<String> updateUser(@RequestBody UserDTO userDTO, @PathVariable Long id) {
         try {
--- UserService/src/main/java/com/dmh/UserService/service/imp/UserServiceImpl.java
+++ UserService/src/main/java/com/dmh/UserService/service/imp/UserServiceImpl.java
@@ -6,36 +6,40 @@ import com.dmh.UserService.repository.UserRepository;
 import com.dmh.UserService.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
-
 @RequiredArgsConstructor
-public class UserServiceImpl implements IUserService{
-    private final UserRepository usersRepository;
-
+public class UserServiceImpl implements IUserService {
+    private final UserRepository userRepository;
 
     public void createUser(UserDTO userDTO) {
-        if (usersRepository.existsByEmail(userDTO.getEmail())) {
+        if (userRepository.existsByEmail(userDTO.getEmail())) {
             throw new RuntimeException("Email already exists");
         }
-        if (usersRepository.existsByDni(userDTO.getDni())) {
+        if (userRepository.existsByDni(userDTO.getDni())) {
             throw new RuntimeException("Dni already exists");
         }
 
         User user = new User();
-        updateUser(userDTO, user.getId());
-
-
-    }
-
-    public void updateUser(UserDTO userDTO, Long id) {
-
-        User user = usersRepository.getUserById(id).orElseThrow(() -> new RuntimeException("User not found"));
         user.setFirstName(userDTO.getFirstName());
         user.setLastName(userDTO.getLastName());
         user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        user.setCvu(userDTO.getCvu());
+        user.setAlias(userDTO.getAlias());
 
-        usersRepository.save(user);
+        userRepository.save(user);
+    }
+
+    public void updateUser(UserDTO userDTO, Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword()); // Assumiendo que el password puede actualizarse
+        user.setCvu(userDTO.getCvu());
+        user.setAlias(userDTO.getAlias());
+
+        userRepository.save(user);
     }
 }

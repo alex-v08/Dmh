bf57ba34091b86c93f303072b0e4a852826cd39b
alex-v08 <alex.velasquez08@outlook.com>
2024-06-26 15:04:54 -0300

    GenerateCvu

---
+++ GenerateCvu/src/main/java/com/dmh/GenerateCvu/GenerateCvuApplication.java
@@ -0,0 +1,13 @@
+package com.dmh.GenerateCvu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateCvuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateCvuApplication.class, args);
+	}
+
+}
---
+++ GenerateCvu/src/test/java/com/dmh/GenerateCvu/GenerateCvuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmh.GenerateCvu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateCvuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

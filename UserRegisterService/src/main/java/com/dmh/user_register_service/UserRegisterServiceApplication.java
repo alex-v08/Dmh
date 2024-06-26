40bd66ffc6787b111a7fc5e76a1752cb511eb59c
alex-v08 <alex.velasquez08@outlook.com>
2024-06-26 09:49:44 -0300

    UserRegisterService

---
+++ UserRegisterService/src/main/java/com/dmh/user_register_service/UserRegisterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dmh.user_register_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegisterServiceApplication.class, args);
+	}
+
+}
